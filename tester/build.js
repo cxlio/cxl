91f88d00de7003f5f23587ba37dd0da85fd3a2d3
Giancarlo Bellido <giancarlo@coaxialhost.com>
2019-12-29 00:26:38 -0500

    chore: cleanup

--- tester/build.js
+++ tester/build.js
@@ -2,11 +2,5 @@ const { build, tsconfig, typescript, pkg, file } = require('../dist/build');
 
 build({
 	outputDir: '../dist/tester',
-	tasks: [
-		tsconfig('tsconfig.json'),
-		// tsconfig('tsconfig.runner.json'),
-		tsconfig('tsconfig.browser.json'),
-		file('puppeteer-runtime.js'),
-		pkg()
-	]
+	tasks: [tsconfig('tsconfig.json'), tsconfig('tsconfig.browser.json'), pkg()]
 });
