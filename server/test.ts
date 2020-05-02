c9c8852a14dc42ddc6abbf3bf6d7c679b2760d2e
Giancarlo Bellido <giancarlo@coaxialhost.com>
2020-05-02 11:58:14 -0400

    test: server tests

---
+++ server/test.ts
@@ -0,0 +1,16 @@
+import { Application } from './index.js';
+import { suite } from '../spec/index.js';
+
+export default suite('server', test => {
+	test('Application', a => {
+		class TestApp extends Application {
+			name = 'test';
+			run() {
+				a.equal(this.name, 'test');
+			}
+		}
+		const app = new TestApp();
+		const done = a.async();
+		app.start().then(done);
+	});
+});
