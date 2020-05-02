f943cbe0e8eff5b68d37bb0409bf872a7688b96f
Giancarlo Bellido <giancarlo@coaxialhost.com>
2020-05-02 11:58:06 -0400

    test: server tests

---
+++ rx/test/from.ts
@@ -0,0 +1,22 @@
+import { suite } from '../../spec/index.js';
+import { from } from '../index';
+
+export default suite('from', test => {
+	test('should create an observable from an array', a => {
+		let current = 10,
+			count = 0;
+		const done = a.async();
+
+		from([10, 20, 30]).subscribe({
+			next(val) {
+				a.equal(val, current);
+				current += 10;
+				count++;
+			},
+			complete() {
+				a.equal(count, 3);
+				done();
+			},
+		});
+	});
+});
