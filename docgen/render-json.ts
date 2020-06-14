c981d13b44bb04368f447d05efd3882586c69caa
Giancarlo Bellido <giancarlo@coaxialhost.com>
2020-06-14 11:58:25 -0400

    feat(docgen): render json

---
+++ docgen/render-json.ts
@@ -0,0 +1,34 @@
+import type { DocGen, File } from './index.js';
+import { Kind, Output } from '../dts/index.js';
+import { relative } from 'path';
+
+const cwd = process.cwd();
+
+function serialize(key: string, value: any) {
+	if (key === 'source') {
+		const pos = value.sourceFile.getLineAndCharacterOfPosition(value.index);
+		return {
+			fileName: relative(cwd, value.sourceFile.fileName),
+			line: pos.line,
+			ch: pos.character,
+		};
+	}
+
+	if (value && value.kind === Kind.Reference)
+		return {
+			id: value.type.id,
+			kind: Kind.Reference,
+			typeParameters: value.typeParameters,
+		};
+
+	return value;
+}
+
+export function render(_app: DocGen, output: Output): File[] {
+	return [
+		{
+			name: 'docs.json',
+			content: JSON.stringify({ modules: output.modules }, serialize, 2),
+		},
+	];
+}
