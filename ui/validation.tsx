0011ea3ac468254b70b64e0d4192dd1c53d671a1
Giancarlo Bellido <giancarlo@coaxialhost.com>
2020-08-02 18:41:03 -0400

    feat(ui): add validation module

---
+++ ui/validation.tsx
@@ -0,0 +1,45 @@
+import { get } from '../component/index.js';
+import type { InputBase } from './form.js';
+
+export type ValidateFunction<T> = (val: T) => string | true;
+
+export function validate<T extends InputBase>(
+	el: T,
+	...validators: ValidateFunction<T['value']>[]
+) {
+	return get(el, 'value').tap(value => {
+		validators.find(validateFn => {
+			const message = validateFn(value);
+			el.invalid = message !== true;
+			if (message !== true)
+				return ((el as any).validationMessage = message);
+		});
+	});
+}
+
+export function $validate<T extends InputBase>(
+	...validators: ValidateFunction<T['value']>[]
+) {
+	return (el: T) => validate(el, ...validators);
+}
+
+export const ValidationMessage = {
+	json: 'Invalid JSON value',
+	zipcode: 'Please enter a valid zip code',
+	equalTo: 'Values do not match',
+	required: 'This field is required',
+	notZero: 'Value cannot be zero',
+	email: 'Please enter a valid email address',
+};
+
+export function required(val: any) {
+	return (
+		(val !== null && val !== undefined && val !== '' && val !== false) ||
+		ValidationMessage.required
+	);
+}
+
+const EMAIL = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+export function email(val: string) {
+	return val === '' || EMAIL.test(val) || ValidationMessage.email;
+}
