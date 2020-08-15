03c65c3c941e484fb7127efff3d9f08995effc1c
Giancarlo Bellido <giancarlo@coaxialhost.com>
2020-08-15 13:54:36 -0400

    feat(ui): minify icons module

--- ui/build.js
+++ ui/build.js
@@ -19,6 +19,7 @@ buildCxl(
 		tasks: [
 			concat(
 				tsconfig('tsconfig.bundle.json'),
+				file('../dist/ui/icons.js').pipe(minify()),
 				file('../dist/ui/index.bundle.js').pipe(minify())
 			),
 		],
