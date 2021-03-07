9f970c5f83d13b2e54883c5034ea5d82a0747111
Giancarlo Bellido <giancarlo@coaxialhost.com>
2021-03-07 11:20:16 -0500

    feat(ui): publish 3.0 version

---
+++ ui/button.tsx
@@ -0,0 +1,120 @@
+///<amd-module name="@cxl/ui/button.js"/>
+import {
+	Attribute,
+	Augment,
+	Component,
+	Slot,
+	Span,
+	StyleAttribute,
+} from '@cxl/component';
+import { dom } from '@cxl/tsx';
+import { css } from '@cxl/css';
+import { Focusable } from '@cxl/template';
+import { on, remove, trigger } from '@cxl/dom';
+import { ButtonBase, FocusHighlight } from './core.js';
+import { CloseIcon } from './icon.js';
+
+/**
+ * Buttons allow users to take actions, and make choices, with a single tap.
+ * @example
+ * <cxl-button primary><cxl-icon icon="upload"></cxl-icon> Upload</cxl-button>
+ * <cxl-button secondary>Secondary</cxl-button>
+ * <cxl-button disabled>Disabled</cxl-button>
+ * <cxl-button flat>Flat Button</cxl-button>
+ * <cxl-button outline>With Outline</cxl-button>
+ */
+@Augment('cxl-button', Slot)
+export class Button extends ButtonBase {}
+
+/**
+ * Chips are compact elements that represent an input, attribute, or action.
+ * @example
+ * <cxl-chip>Single Chip</cxl-chip>
+ * <cxl-chip secondary removable>Removable Chip</cxl-chip>
+ * <cxl-chip><cxl-avatar small></cxl-avatar> Chip with Avatar</cxl-chip>
+ */
+@Augment<Chip>(
+	'cxl-chip',
+	Focusable,
+	css({
+		$: {
+			borderRadius: 16,
+			font: 'subtitle2',
+			backgroundColor: 'onSurface12',
+			display: 'inline-flex',
+			color: 'onSurface',
+			height: 32,
+			verticalAlign: 'top',
+			alignItems: 'center',
+		},
+		$primary: {
+			color: 'onPrimary',
+			backgroundColor: 'primary',
+		},
+		$secondary: {
+			color: 'onSecondary',
+			backgroundColor: 'secondary',
+		},
+		$small: { font: 'caption', lineHeight: 20, height: 20 },
+		content: {
+			display: 'inline-block',
+			marginLeft: 12,
+			paddingRight: 12,
+		},
+		avatar: { display: 'inline-block' },
+		remove: {
+			display: 'none',
+			marginRight: 8,
+			cursor: 'pointer',
+		},
+		remove$removable: {
+			display: 'inline-block',
+		},
+		...FocusHighlight,
+	}),
+	$ => (
+		<>
+			<span className="avatar">
+				<$.Slot selector="cxl-avatar" />
+			</span>
+			<span className="content">
+				<slot />
+			</span>
+		</>
+	),
+	host => (
+		<Span
+			$={el => on(el, 'click').tap(() => host.remove())}
+			className="remove"
+		>
+			<CloseIcon width={16} />
+		</Span>
+	),
+	host =>
+		on(host, 'keydown').tap(ev => {
+			if (
+				host.removable &&
+				(ev.key === 'Delete' || ev.key === 'Backspace')
+			)
+				host.remove();
+		})
+)
+export class Chip extends Component {
+	@StyleAttribute()
+	removable = false;
+	@StyleAttribute()
+	disabled = false;
+	@Attribute()
+	touched = false;
+	@StyleAttribute()
+	primary = false;
+	@StyleAttribute()
+	secondary = false;
+	@StyleAttribute()
+	small = false;
+
+	remove() {
+		remove(this);
+		trigger(this, 'cxl-chip.remove');
+	}
+}
