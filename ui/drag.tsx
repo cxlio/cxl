e7ff8b39c87a4af69594944139fb8d456356ecda
Giancarlo Bellido <giancarlo@coaxialhost.com>
2021-07-10 13:00:08 -0400

    feat(ui): drag module

---
+++ ui/drag.tsx
@@ -0,0 +1,41 @@
+import { Augment, Component } from '@cxl/component';
+import { css } from '@cxl/css';
+import { dom } from '@cxl/tsx';
+import { dropTarget } from '@cxl/drag';
+import { trigger } from '@cxl/dom';
+
+@Augment<DragContainer>(
+	'cxl-drop-container',
+	css({
+		$: {
+			display: 'block',
+		},
+		over$dragover: {
+			display: 'contents',
+		},
+		over: { display: 'none' },
+		notover$dragover: { display: 'none' },
+	}),
+	$ =>
+		dropTarget($).tap(ev => {
+			if (ev.dataTransfer) {
+				const items = ev.dataTransfer.items;
+				$.$items = Array.from(items);
+				trigger($, 'change');
+			}
+		}),
+	() => (
+		<>
+			<slot className="over" name="over" />
+			<slot className="notover" name="notover" />
+			<slot />
+		</>
+	)
+)
+export class DragContainer extends Component {
+	$items: DataTransferItem[] = [];
+
+	get items(): DataTransferItem[] {
+		return this.$items;
+	}
+}
